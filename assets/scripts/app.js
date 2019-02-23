2d6936f41611d0e2e714c783242198145abb15f2
IIZODIACII <amr_x14@outlook.com>
2019-02-23 18:22:42 +0200

    Added the News API

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,23 @@
 const apiBtns = document.querySelectorAll(".api-btn");
 
+var news = false;
+var jokes = false;
+var twitter = false;
+
 apiBtns.forEach(btn =>
   btn.addEventListener("change", e => {
     console.log(e.target.dataset.name); // API name
     if (e.target.checked) {
-      // TURN ON API
+      // TURN ON API 
+      if (e.target.id == 1){
+        news = true;
+      }
+      if (e.target.id == 2){
+        jokes = true;
+      }
+      if (e.target.id == 3){
+        twitter = true;
+      }
       console.log("Checked");
     } else {
       // TURN OFF API
@@ -12,3 +25,28 @@ apiBtns.forEach(btn =>
     }
   })
 );
+
+function UserAction() {
+  if (news)
+  {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             var data = JSON.parse(this.responseText);
+             console.log(data.articles[0].url);
+             
+         }
+    };
+    xhttp.open("GET", "https://newsapi.org/v2/top-headlines?country=eg&apiKey=5546e675c6334e0b9b1bf229e18a2d13", true);
+    xhttp.send();
+  }
+  if (jokes)
+  {
+    // get jokes
+  }
+  if (twitter)
+  {
+    // TWEET
+  }
+  
+}
