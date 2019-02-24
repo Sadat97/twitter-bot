522f6457e861a7e43dfa07fe294314c23901e191
Mohamed Anwar <moanwar1997@gmail.com>
2019-02-24 19:37:28 +0200

    functioning tweeting api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,63 +7,93 @@ var url;
 var jk;
 
 apiBtns.forEach(btn =>
-  btn.addEventListener("change", e => {
-    console.log(e.target.dataset.name); // API name
-    if (e.target.checked) {
-      // TURN ON API 
-      if (e.target.id == 1) {
-        news = true;
-      }
-      if (e.target.id == 2) {
-        jokes = true;
-      }
-      if (e.target.id == 3) {
-        twitter = true;
-      }
-      console.log("Checked");
-    } else {
-      // TURN OFF API
-      if (e.target.id == 1) {
-        news = false;
-      }
-      if (e.target.id == 2) {
-        jokes = false;
-      }
-      if (e.target.id == 3) {
-        twitter = false;
-      }
-      console.log("Not checked");
-    }
-  })
+    btn.addEventListener("change", e => {
+        console.log(e.target.dataset.name); // API name
+        if (e.target.checked) {
+            // TURN ON API
+            if (e.target.id == 1) {
+                news = true;
+            }
+            if (e.target.id == 2) {
+                jokes = true;
+            }
+            if (e.target.id == 3) {
+                twitter = true;
+            }
+            console.log("Checked");
+        } else {
+            // TURN OFF API
+            if (e.target.id == 1) {
+                news = false;
+            }
+            if (e.target.id == 2) {
+                jokes = false;
+            }
+            if (e.target.id == 3) {
+                twitter = false;
+            }
+            console.log("Not checked");
+        }
+    })
 );
 
 function UserAction() {
-  if (news) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        url = JSON.parse(this.responseText);
-        console.log(url.articles[0].url);
+    var news_link = "";
+    if (news) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                url = JSON.parse(this.responseText);
+                console.log(url.articles[0].url);
+                news_link = url.articles[0].url;
 
-      }
+            }
+        };
+        xhttp.open("GET", "https://newsapi.org/v2/top-headlines?country=eg&apiKey=5546e675c6334e0b9b1bf229e18a2d13", false);
+        xhttp.send();
+    }
+    if (jokes) {
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                jk = JSON.parse(this.responseText);
+                console.log(jk);
+            }
+        };
+        req.open("GET", "https://geek-jokes.sameerkumar.website/api", false);
+        req.send();
+    }
+    if (twitter) {
+
+    }
+    Promise.all([]).then(function (values) {
+        console.log(values);
+    });
+
+    var status = `Joke of the day: ${jk}
+
+top headline of the day: ${news_link}`;
+
+        tweetIt(status);
+}
+
+function tweetIt(status) {
+    var param = {
+        tweet:
+            {
+                status: status
+            }
     };
-    xhttp.open("GET", "https://newsapi.org/v2/top-headlines?country=eg&apiKey=5546e675c6334e0b9b1bf229e18a2d13", true);
-    xhttp.send();
-  }
-  if (jokes) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        jk = JSON.parse(this.responseText);
-        console.log(jk);
-
-      }
-    };
-    req.open("GET", "https://geek-jokes.sameerkumar.website/api", true);
-    req.send();
-  }
-  if (twitter) {
-
-  }
-
+    var url = "http://localhost:3000/tweets";
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(param),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then((response) => response.arrayBuffer())
+        .then((data) => {
+            console.log(data);
+        });
 }
